3c5454be46702a7b9b621f3213f42944ee22643c
Leonard <Leonardhernandez02@gmail.com>
2024-04-21 18:59:34 -0500

    creando clase model y repository

---
+++ src/main/java/com/leonard/springboot/di/app/springbootdi/repositories/ProductRepository.java
@@ -0,0 +1,26 @@
+package com.leonard.springboot.di.app.springbootdi.repositories;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.leonard.springboot.di.app.springbootdi.models.Product;
+
+public class ProductRepository {
+
+    private List<Product> data;
+    
+    public ProductRepository(List<Product> data) {
+        this.data = Arrays.asList(
+            new Product(1L, "Memoria corsair 32", 300L), 
+            new Product(2L, "Cpu intel core i9", 850L),
+            new Product(3L, "Teclado razer mini 60%", 18L),
+            new Product(4L, "Motherboard gigabyte", 490L)
+        );
+    }
+
+    public List<Product> findAll() {
+        return this.data;
+    }
+    
+
+}
