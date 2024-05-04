7e94de147129587e97da0de94e08c35abdaf8323
Leonard <Leonardhernandez02@gmail.com>
2024-05-03 22:09:23 -0500

    inyectando mediante interfases

--- src/main/java/com/leonard/springboot/di/app/springbootdi/controllers/SomeController.java
+++ src/main/java/com/leonard/springboot/di/app/springbootdi/controllers/SomeController.java
@@ -9,14 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.leonard.springboot.di.app.springbootdi.models.Product;
-import com.leonard.springboot.di.app.springbootdi.services.ProductServiceImpl;
+import com.leonard.springboot.di.app.springbootdi.services.ProductService;
+
 
 @RestController
 @RequestMapping("/api")
 public class SomeController {
 
     @Autowired
-    private ProductServiceImpl service;
+    private ProductService service;
     @GetMapping()
     public List<Product> list(){
         return service.findAll();
