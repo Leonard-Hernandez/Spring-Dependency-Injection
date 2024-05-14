36eba38cb34b4f2e28d3ec634e17842126efdbde
Leonard <Leonardhernandez02@gmail.com>
2024-05-13 20:18:00 -0500

    Usando la anotacion cualifier

---
+++ src/main/java/com/leonard/springboot/di/app/springbootdi/repositories/PeoductRepositoryFoo.java
@@ -0,0 +1,27 @@
+package com.leonard.springboot.di.app.springbootdi.repositories;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+
+import com.leonard.springboot.di.app.springbootdi.models.Product;
+
+@Primary
+@Repository
+public class PeoductRepositoryFoo implements ProductRepository{
+
+    @Override
+    public List<Product> findAll() {
+        return Collections.singletonList(new Product(1L, "monitor asus 27",600l));         
+    }
+
+    @Override
+    public Product findById(Long id) {
+        return new Product(1L, "monitor asus 27",600l);
+    }
+
+
+
+}
