299c9942cb6a903326bf3ab23fbaa37743327116
Leonard <Leonardhernandez02@gmail.com>
2024-09-06 18:36:47 -0500

    creando clases a partir de un archivo json

---
+++ src/main/java/com/leonard/springboot/di/app/springbootdi/repositories/ProductRepositoryJson.java
@@ -0,0 +1,39 @@
+package com.leonard.springboot.di.app.springbootdi.repositories;
+
+import java.util.List;
+import java.io.IOException;
+import java.util.Arrays;
+
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.leonard.springboot.di.app.springbootdi.models.Product;
+
+public class ProductRepositoryJson implements ProductRepository{
+
+    private List<Product> list;
+
+    public ProductRepositoryJson() {
+        Resource resource = new ClassPathResource("json/product.json");
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            list = Arrays.asList(objectMapper.readValue(resource.getFile(), Product[].class));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return list;
+    }
+
+    @Override
+    public Product findById(Long id) {
+        return null;
+    }
+
+}
